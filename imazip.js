fd5c2cc28d8bc509f518e17ba6752483a2dc1ed1
hatz48 <hatz48@hatena.ne.jp>
2014-02-09 18:38:46 +0900

    compile

--- imazip.js
+++ imazip.js
@@ -1,4 +1,75 @@
-/// <reference path="d.ts/DefinitelyTyped/jquery/jquery.d.ts" />
+
+var ImageSelector = (function () {
+    function ImageSelector(args) {
+        var _this = this;
+        this.urls = args.urls;
+        this.callbacks = {
+            onEnd: function (e) {
+                _this.onEnd(e);
+            },
+            onClickClose: function (e) {
+                _this.onClickClose(e);
+            },
+            onClickDownload: function (e) {
+                _this.onClickDownload(e);
+            },
+            onClickImage: function (e) {
+                _this.onClickImage(e);
+            }
+        };
+
+        this.$el = $(JST['image-container.ejs']({
+            urls: this.urls,
+            title: $('title').text()
+        }));
+        $(document.body).append(this.$el);
+
+        $(document.body).on('imazip:end', this.callbacks.onEnd);
+        this.$el.on('click', '.imazip-download-button.close', this.callbacks.onClickClose);
+        this.$el.on('click', '.imazip-download-button.download', this.callbacks.onClickDownload);
+        this.$el.on('click', '.imazip-image-container', this.callbacks.onClickImage);
+    }
+    ImageSelector.prototype.destroy = function () {
+        this.$el.remove();
+        $(document.body).off('imazip:end', this.callbacks.onEnd);
+    };
+
+    ImageSelector.prototype.onEnd = function (e) {
+        this.destroy();
+    };
+
+    ImageSelector.prototype.onClickDownload = function (e) {
+        var filename = this.$el.find('.imazip-download-filename').val();
+        var urls = Array.prototype.map.call(this.$el.find('.imazip-image-container.checked img'), function (img) {
+            return $(img).attr('src');
+        }).filter(function (url) {
+            return url ? 1 : 0;
+        });
+        chrome.runtime.sendMessage({
+            name: "imazip",
+            page_url: location.href,
+            urls: urls,
+            filename: filename
+        }, function (res) {
+            console.log(res);
+            this.$el.remove();
+        });
+    };
+
+    ImageSelector.prototype.onClickImage = function (e) {
+        $(e.currentTarget).toggleClass('checked');
+    };
+
+    ImageSelector.prototype.onClickClose = function (e) {
+        this.destroy();
+        chrome.runtime.sendMessage({
+            name: "imazip:close"
+        }, function (res) {
+            console.log(res);
+        });
+    };
+    return ImageSelector;
+})();
 
 var UrlPicker = (function () {
     function UrlPicker() {
@@ -13,12 +84,16 @@ var UrlPicker = (function () {
             },
             onClick: function (e) {
                 return _this.onClick(e);
+            },
+            onEnd: function (e) {
+                return _this.onEnd(e);
             }
         };
 
         $(document.body).on('mouseover', this.callbacks.onMouseover);
         $(document.body).on('mouseout', this.callbacks.onMouseout);
         $(document.body).on('click', this.callbacks.onClick);
+        $(document.body).on('imazip:end', this.callbacks.onEnd);
     }
     UrlPicker.prototype.onMouseover = function (e) {
         var target = this.highlightTarget = e.target;
@@ -32,38 +107,53 @@ var UrlPicker = (function () {
     };
 
     UrlPicker.prototype.onClick = function (e) {
-        var urls = Array.prototype.map.call($(e.target).find('img'), function (img) {
+        var urls = Array.prototype.filter.call($(e.target).find('img'), function (img) {
+            return (img.width >= 300 && img.height >= 300) ? 1 : 0;
+        }).map(function (img) {
             var $img = $(img);
             var $a = $(img).closest('a');
-            if (!$img.attr('src').match(/(jpeg|jpg|png|gif)$/i))
+            if (!$img.attr('src'))
                 return;
             return ($a.length > 0 && $a.attr('href').match(/(jpeg|jpg|png|gif)$/i)) ? $a.attr('href') : $(img).attr('src');
         }).filter(function (url) {
             return url ? 1 : 0;
         });
 
+        $(this).trigger('picked', urls);
+
+        new ImageSelector({ urls: urls });
+    };
+
+    UrlPicker.prototype.onEnd = function (e) {
         $(this.highlightTarget).css({ 'box-shadow': this.originalShadow });
         $(document.body).off('mouseover', this.callbacks.onMouseover);
         $(document.body).off('mouseout', this.callbacks.onMouseout);
         $(document.body).off('click', this.callbacks.onClick);
-        $(this).trigger('picked', urls);
+        $(document.body).off('end', this.callbacks.onEnd);
     };
     return UrlPicker;
 })();
 
 (function () {
-    var picker = new UrlPicker();
-    $(picker).on('picked', function (e) {
-        var urls = [];
-        for (var _i = 0; _i < (arguments.length - 1); _i++) {
-            urls[_i] = arguments[_i + 1];
-        }
-        chrome.runtime.sendMessage({
-            name: "imazip",
-            page_url: location.href,
-            urls: urls
-        }, function (res) {
-            // response handle
+    chrome.runtime.onConnect.addListener(function (port) {
+        console.log(port);
+        console.assert(port.name == "imazip");
+
+        port.onMessage.addListener(function (msg) {
+            console.log(msg);
+            if (msg.command === 'imazip:start') {
+                var picker = new UrlPicker();
+                $(picker).on('picked', function (e) {
+                    var urls = [];
+                    for (var _i = 0; _i < (arguments.length - 1); _i++) {
+                        urls[_i] = arguments[_i + 1];
+                    }
+                    $(document.body).trigger('imazip:end');
+                });
+            }
+            if (msg.command === 'imazip:end') {
+                $(document.body).trigger('imazip:end');
+            }
         });
     });
 })();
